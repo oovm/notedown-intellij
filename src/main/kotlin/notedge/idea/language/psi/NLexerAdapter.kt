60aa803bf249b44641eccd08d56240e806672ff4
Aster <galaster@foxmail.com>
2022-07-08 21:28:47 +0800

    Solve the nesting problem of asterisks

--- src/main/kotlin/notedge/idea/language/psi/NLexerAdapter.kt
+++ src/main/kotlin/notedge/idea/language/psi/NLexerAdapter.kt
@@ -2,11 +2,50 @@ package notedge.idea.language.psi
 
 
 import com.intellij.lexer.FlexAdapter
-import com.intellij.lexer.Lexer
-import com.intellij.lexer.LexerPosition
+import com.intellij.lexer.LexerBase
 import com.intellij.psi.tree.IElementType
 import notedge.idea._NotedownLexer
 
 class NLexerAdapter : FlexAdapter(_NotedownLexer()) {
+
+}
+
+enum class LexerState {
+
+}
+
+class NLexerCustom : LexerBase() {
+
+    override fun start(buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getState(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTokenType(): IElementType? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTokenStart(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTokenEnd(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun advance() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBufferSequence(): CharSequence {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBufferEnd(): Int {
+        TODO("Not yet implemented")
+    }
 
 }
