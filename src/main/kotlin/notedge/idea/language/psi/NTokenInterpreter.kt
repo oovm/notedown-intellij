f80872c3e8fb7e1a15aa34bdf8c167a4eb87c2d4
Aster <galaster@foxmail.com>
2022-07-09 01:11:15 +0800

    Fix newline break

--- src/main/kotlin/notedge/idea/language/psi/NTokenInterpreter.kt
+++ src/main/kotlin/notedge/idea/language/psi/NTokenInterpreter.kt
@@ -1,5 +1,9 @@
 package notedge.idea.language.psi
 
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
+
+@OptIn(ExperimentalStdlibApi::class)
 @Suppress("MemberVisibilityCanBePrivate")
 class NTokenInterpreter(val buffer: CharSequence, var startOffset: Int, val endOffset: Int, var context: Int) {
     //URL= [A-Za-z0-9]+:"//"[\-\p{XID_Continue}./?&#]+
@@ -14,53 +18,144 @@ class NTokenInterpreter(val buffer: CharSequence, var startOffset: Int, val endO
         private val EOL = "\\R".toRegex()
         private val WHITE_SPACE = "[^\\S\\r\\n]+".toRegex()
         private val NEW_LINE = "[\\r\\n]".toRegex()
-        private val ESCAPE = "\\\\".toRegex()
+        private val HEADER_HASH = "#+".toRegex()
         private val ASTERISK = "[*]+".toRegex()
         private val TILDE = "~+".toRegex()
-        private val HEADER_HASH = "#+".toRegex()
+        private val ESCAPE = "\\\\".toRegex()
+        private val SYMBOL = "[\\p{ASCII}_][\\p{ASCII}_]*".toRegex()
     }
 
     private var stack: MutableList<StackItem> = mutableListOf()
 
-    @OptIn(ExperimentalStdlibApi::class)
     fun interpreter(): MutableList<StackItem> {
-        var res: MatchResult? = null;
         while (startOffset < endOffset) {
-            res = WHITE_SPACE.matchAt(buffer, startOffset);
-            if (res != null) {
-                checkEscape(res)
-                continue
-            }
-            res = ESCAPE.matchAt(buffer, startOffset);
-            if (res != null) {
-                checkEscape(res)
-                continue
-            }
-            res = HEADER_HASH.matchAt(buffer, startOffset);
-            if (res != null) {
-                checkHeadHash(res)
-                continue
-            }
+            if (matchesWhitespace()) continue
+            if (matchesNewline()) continue
+            if (matchesHeadHash()) continue
+            if (matchesEscape()) continue
+            if (matchesSymbol()) continue
             break
         }
         checkRest()
         return stack
     }
 
-    fun checkHeadHash(r: MatchResult) {
-        if (startOffset < endOffset) {
-            stack.add(StackItem(NoteTypes.PLAIN_TEXT, startOffset, endOffset, context))
-        }
+    fun matchesWhitespace(): Boolean {
+        val r = WHITE_SPACE.matchAt(buffer, startOffset) ?: return false;
+        stack.add(StackItem(TokenType.WHITE_SPACE, r.range.first, r.range.last, context))
+        return addOffset(r)
     }
 
-    fun checkEscape(r: MatchResult) {
+    fun matchesNewline(): Boolean {
+        val r = NEW_LINE.matchAt(buffer, startOffset) ?: return false;
+        if (shouldBreakHeader()) {
+            stack.add(StackItem(NoteTypes.BREAK_PART, r.range.first, r.range.last, context))
+        } else {
+            stack.add(StackItem(NoteTypes.NEW_LINE, r.range.first, r.range.last, context))
+        }
+        return addOffset(r)
+    }
+
+    fun matchesHeadHash(): Boolean {
+        val r = HEADER_HASH.matchAt(buffer, startOffset) ?: return false;
+        stack.add(StackItem(NoteTypes.HEADER_HASH, r.range.first, r.range.last, context))
+        return addOffset(r)
+    }
+
+    fun matchesEscape(): Boolean {
+        val r = ESCAPE.matchAt(buffer, startOffset) ?: return false;
         stack.add(StackItem(NoteTypes.ESCAPE, r.range.first, r.range.last, context))
-        startOffset = r.range.last + 1
+        return addOffset(r)
+    }
+
+    fun matchesSymbol(): Boolean {
+        val r = SYMBOL.matchAt(buffer, startOffset) ?: return false;
+        when {
+            lastIs(NoteTypes.ESCAPE) -> {
+                stack.add(StackItem(NoteTypes.SYMBOL, r.range.first, r.range.last, context))
+            }
+
+            else -> {
+                stack.add(StackItem(NoteTypes.PLAIN_TEXT, r.range.first, r.range.last, context))
+            }
+        }
+        return addOffset(r)
+    }
+
+    fun matchesAsterisk(): Boolean {
+        val r = ASTERISK.matchAt(buffer, startOffset) ?: return false;
+        val last = lastAsterisk();
+//        if (last == null || last > length) {
+//            when (length) {
+//                1 -> {
+//                    lookAheadBuffer.add(NoteTypes.ITALIC_L)
+//                }
+//                2 -> {
+//                    lookAheadBuffer.add(NoteTypes.BOLD_L)
+//                }
+//                3 -> {
+//                    lookAheadBuffer.add(NoteTypes.ITALIC_BOLD_L)
+//                }
+//                else -> {
+//                    lookAheadBuffer.add(NoteTypes.PLAIN_TEXT)
+//                    return lastLookAhead
+//                }
+//            }
+//            // stack.add(StackItem.Asterisk(length))
+//
+//        }
+//        // if (last == here.yylength())
+//        else {
+//            when (length) {
+//                1 -> {
+//                    asteriskStack.removeLast()
+//                    lookAheadBuffer.add(NoteTypes.ITALIC_R)
+//                }
+//                2 -> {
+//                    asteriskStack.removeLast()
+//                    lookAheadBuffer.add(NoteTypes.BOLD_R)
+//                }
+//                3 -> {
+//                    asteriskStack.removeLast()
+//                    lookAheadBuffer.add(NoteTypes.ITALIC_BOLD_R)
+//                }
+//                else -> TODO("unreachable")
+//            }
+//        }
+        return addOffset(r)
     }
 
     fun checkRest() {
         if (startOffset < endOffset) {
             stack.add(StackItem(NoteTypes.PLAIN_TEXT, startOffset, endOffset, context))
         }
+    }
+
+    fun lastIs(token: IElementType): Boolean = when (val last = stack.lastOrNull()) {
+        null -> false
+        else -> last.token == token
+    }
+
+    fun lastAsterisk(): Boolean {
+//        for (item in stack.reversed()) {
+//            if (item.token == NoteTypes.ITALIC_L) {
+//                return true
+//            }
+//        }
+        TODO()
+    }
+
+    fun shouldBreakHeader(): Boolean {
+        for (item in stack.reversed()) {
+            if (item.token == NoteTypes.HEADER_HASH) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun addOffset(r: MatchResult): Boolean {
+        startOffset = r.range.last + 1
+        return true
     }
 }
